27a1210046d45e4a61742319c547271336388885
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2021-02-11 19:01:28 +0300

    trying solve a problem with depth(it`s undefined)

---
+++ app/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <TreeBrowser :node="root" />
+  </div>
+</template>
+
+<script>
+import TreeBrowser from "./components/TreeBrowser.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      root: {
+        name: "/",
+        children: [
+          {
+            name: "music",
+            children: [
+              {
+                name: "song.mp3",
+              }
+            ]
+          },
+          {
+            name: "work space",
+            children: [
+              {
+                name: "sourse.js",
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    TreeBrowser
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/TreeBrowser.vue
@@ -0,0 +1,36 @@
+<template >
+  <div>
+    <div :style="{ 'margin-left': `${depth * 20}px` }" class="node">
+      {{ node.name }}
+    </div>
+    <TreeBrowser
+      v-for="child in node.children"
+      :key="child.name"
+      :node="child"
+      :depth="1 + depth"
+    />{{ typeof depth  }}
+  </div>
+</template>
+<script>
+export default {
+  name: "TreeBrowser",
+  props: {
+    node: Object,
+    depth: {
+      type: Number,
+      dafault: 1,
+    },
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+};
+</script>
+<style scoped>
+.node {
+  text-align: left;
+  font-size: 15px;
+}
+</style>
